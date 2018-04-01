70a2d0d12398965b230cdc10b5086ff5b4013ca9
ChoYongHyeon <eddy1001@kookmin.ac.kr>
2018-04-01 18:52:35 +0900

    가청 dB의 수치의 첫 값을 주요 주파수로 설정하여 그 주파수 범위를 if문으로 구분하여 음계를 list로 출력하는 코드

--- script/FftCode.py
+++ script/FftCode.py
@@ -2,31 +2,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy.io import wavfile # get the api
-
 fs, data = wavfile.read('Air.wav') # load the data
 y = data.T[0] # this is a two channel soundtrack, I get the first track
 time = data.shape[0] / rate
 
-
 blockSize = int(fs / 10)
 blockHall = int(len(y) / blockSize)
+
+ans = []
 
 for i in range(0, blockHall):
     part = y[blockSize * i: blockSize * (i+1)]
     n=len(part)        # Length of signal
     NFFT=n      # ?? NFFT=2^nextpow2(length(y))  ??
     k=np.arange(NFFT)
+
     f0=k*fs/NFFT    # double sides frequency range
     f0=f0[range(int(NFFT/2))]
 
     Y=np.fft.fft(part)/NFFT        # fft computing and normaliation
     Y=Y[range(int(NFFT/2))]          # single sied frequency range
+
     amplitude_Hz = 2*abs(Y)
     phase_ang = np.angle(Y)*180/np.pi
+
+    bank = np.where((amplitude_Hz >= (amplitude_Hz.max() / 4)) & (amplitude_Hz > 250))
+
+    result = '침묵'
+
+    check = 0
     
+    if (len(bank[0]) > 1):
+        noteName = ['도', '레', '미', '파', '솔', '라', '시']
+
+        check = f0[bank[0][0]]
+
+        if (check > 127 and check <= 131):
+            result = noteName[0]
+        elif (check > 131 and check <= 147):
+            result = noteName[1]
+        elif (check > 147 and check <= 165):
+            result = noteName[2]
+        elif (check > 165 and check <= 175):
+            result = noteName[3]
+        elif (check > 175 and check <= 196):
+            result = noteName[4]
+        elif (check > 196 and check <= 220):
+            result = noteName[5]
+        elif (check > 220 and check <= 247):
+            result = noteName[6]
+
     plt.plot(f0,amplitude_Hz,'r')
     plt.title('Single-Sided Amplitude Spectrum of y(t)' + str(check))
     plt.xlabel('frequency($Hz$)')
     plt.ylabel('amplitude')
     plt.xlim(0,4000)
     plt.show()
+    
+    ans.insert(i, result + str(check))
+    
+ans
