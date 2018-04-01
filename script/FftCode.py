49dcd362e3700f0ac1c1e03347beaa821690e705
ChoYongHyeon <eddy1001@kookmin.ac.kr>
2018-04-01 18:49:16 +0900

    wav를 0.1초 단위로 분할 후 각각 fft를 적용 후 가청 주파수 4000까지만 그래프로 출력하는 코드

--- script/FftCode.py
+++ script/FftCode.py
@@ -7,20 +7,26 @@ fs, data = wavfile.read('Air.wav') # load the data
 y = data.T[0] # this is a two channel soundtrack, I get the first track
 time = data.shape[0] / rate
 
-n = len(y)
-NFFT = n
-k = np.arange(NFFT)
-f0 = k * fs / NFFT
-f0 = f0[range(int(NFFT / 2))]
 
-Y = np.fft.fft(part) / NFFT
-Y = Y[range(int(NFFT / 2))]
+blockSize = int(fs / 10)
+blockHall = int(len(y) / blockSize)
 
-amplitude_Hz = 2 * abs(Y)
-phase_ang = np.angle(Y) * 180 / np.pi
+for i in range(0, blockHall):
+    part = y[blockSize * i: blockSize * (i+1)]
+    n=len(part)        # Length of signal
+    NFFT=n      # ?? NFFT=2^nextpow2(length(y))  ??
+    k=np.arange(NFFT)
+    f0=k*fs/NFFT    # double sides frequency range
+    f0=f0[range(int(NFFT/2))]
 
-plt.plot(f0,amplitude_Hz,'r')
-plt.title('Single-Sided Amplitude Spectrum of y(t)' + str(check))
-plt.xlabel('frequency($Hz$)')
-plt.ylabel('amplitude')
-plt.show()
+    Y=np.fft.fft(part)/NFFT        # fft computing and normaliation
+    Y=Y[range(int(NFFT/2))]          # single sied frequency range
+    amplitude_Hz = 2*abs(Y)
+    phase_ang = np.angle(Y)*180/np.pi
+    
+    plt.plot(f0,amplitude_Hz,'r')
+    plt.title('Single-Sided Amplitude Spectrum of y(t)' + str(check))
+    plt.xlabel('frequency($Hz$)')
+    plt.ylabel('amplitude')
+    plt.xlim(0,4000)
+    plt.show()
