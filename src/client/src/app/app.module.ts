5967e5f52c1d6eba6806043b2aa1be5174a7690c
acoustically <acoustically@kookmin.ac.kr>
2018-04-05 04:12:28 +0900

    add index page - top bar and search bar

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { LogoComponent } from './logo/logo.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    LogoComponent,
+    SearchBarComponent,
+    TopBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
