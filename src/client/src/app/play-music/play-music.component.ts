897d3732b1752127190961b7f1ab35712dfba9aa
acoustically <acoustically@kookmin.ac.kr>
2018-05-29 20:08:04 +0900

    add work - convert notes

--- src/client/src/app/play-music/play-music.component.ts
+++ src/client/src/app/play-music/play-music.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 export class PlayMusicComponent implements OnInit {
 
   notes: any;
+  convertedNotes = [];
 
   constructor() { }
 
@@ -19,11 +20,20 @@ export class PlayMusicComponent implements OnInit {
   ngOnChanges(changes) {
     if(changes.notes.currentValue) {
       alert(JSON.stringify(this.notes));
+      for(let note of this.notes) {
+        let array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];        
+        for(let one of note) {
+          let pitch = one["pitch"];
+          let octave = one["octave"];
+          array[(octave - 4) * 7 + pitch - 1] = 1;
+        }
+        this.convertedNotes.push(array);
+      }
+      alert(JSON.stringify(this.convertedNotes));
     }
   }
 
   clickImage()  {
-    
   }
 
 }
